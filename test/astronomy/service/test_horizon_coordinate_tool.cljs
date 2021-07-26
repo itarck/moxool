1e7e4836411d163037419a88bd43b1bd10945433
itarck <itarck@gmail.com>
2021-07-26 19:16:16 +0800

    测试服务

--- test/astronomy/service/test_horizon_coordinate_tool.cljs
+++ test/astronomy/service/test_horizon_coordinate_tool.cljs
@@ -1,12 +1,12 @@
 (ns astronomy.service.test-horizon-coordinate-tool
   (:require
+   [cljs.test :refer-macros [deftest is run-tests]]
    [posh.reagent :as p]
-   [astronomy.scripts.test-conn :refer [create-test-conn!]]
-   [astronomy.service.horizon-coordinate-tool :as s.horizon-coordinate-tool]
-   [astronomy.app.core :refer [free-app-instance]]))
+   [astronomy.scripts.test-conn :as test-conn]
+   [astronomy.service.horizon-coordinate-tool :as s.horizon-coordinate-tool]))
 
 
-(def conn (create-test-conn!))
+(def conn (test-conn/init-conn!))
 
 (def props {})
 
@@ -14,42 +14,14 @@
   {:conn conn})
 
 
-(let [env {:conn (create-test-conn!)}
-      event #:event {:action :horizon-coordinate/log
-                     :detail {:name "testing"}}]
-  (s.horizon-coordinate-tool/handle-event! {} env event))
+(deftest test-horizon-coordinate-tool
+  (is (= false
+         (let [event1 #:event {:action :horizon-coordinate/change-show-latitude
+                               :detail {:horizon-coordinate {:db/id [:coordinate/name "地平坐标系"]}
+                                        :show? false}}
+               _ (s.horizon-coordinate-tool/handle-event! {} {:conn conn} event1)
+               hc @(p/pull conn '[*] [:coordinate/name "地平坐标系"])]
+           (:horizon-coordinate/show-latitude? hc)))))
 
 
-(let [conn (create-test-conn!)
-      env {:conn conn}
-      event #:event {:action :horizon-coordinate/change-show-latitude
-                     :detail {:horizon-coordinate {:db/id [:coordinate/name "地平坐标系"]}
-                              :show? false}}]
-  (s.horizon-coordinate-tool/handle-event! {} env event)
-  (let [hc @(p/pull conn '[*] [:coordinate/name "地平坐标系"])]
-    (:horizon-coordinate/show-latitude? hc)))
-
-
-;; 闭环测试
-
-(def real-conn (:app/scene-conn free-app-instance))
-
-(def real-env {:conn real-conn})
-
-(def show-latitude-event
-  #:event {:action :horizon-coordinate/change-show-latitude
-           :detail {:horizon-coordinate {:db/id [:coordinate/name "地平坐标系"]}
-                    :show? true}})
-
-(def show-longitude-event
-  #:event {:action :horizon-coordinate/change-show-longitude
-           :detail {:horizon-coordinate {:db/id [:coordinate/name "地平坐标系"]}
-                    :show? false}})
-
-(let [_ (s.horizon-coordinate-tool/handle-event! {} real-env show-latitude-event)
-      hc @(p/pull real-conn '[*] [:coordinate/name "地平坐标系"])]
-  (:horizon-coordinate/show-latitude? hc))
-
-(let [_ (s.horizon-coordinate-tool/handle-event! {} real-env show-longitude-event)
-      hc @(p/pull real-conn '[*] [:coordinate/name "地平坐标系"])]
-  (:horizon-coordinate/show-longitude? hc))
+(run-tests)
