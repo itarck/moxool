3c1ed5b2b6734f7ce85b00f45fc7b268df9ab251
itarck <itarck@gmail.com>
2021-08-04 20:54:49 +0800

    更改cross hair 图标

--- src/astronomy/component/cross_hair.cljs
+++ src/astronomy/component/cross_hair.cljs
@@ -2,15 +2,19 @@
   (:require
    [shu.three.vector3 :as v3]
    [helix.core :refer [defnc $] :as h]
+   ["three" :as three]
    ["react" :as react :refer [Suspense]]
    ["@react-three/drei" :refer [Html Billboard useTexture]]))
 
 
 (defnc CrossHairComponent [props]
   (let [{:keys [position onClick]} props
-        size (* 0.005 (v3/length (v3/from-seq (seq position))))
-        texture1 (useTexture "/image/moxool/crosshair1.png")]
+        size (* 0.015 (v3/length (v3/from-seq (seq position))))
+        texture1 (useTexture "/image/moxool/crosshair.png")]
     ($ Billboard {:args #js [size size]
                   :position position
                   :onClick onClick}
-       ($ :meshStandardMaterial {:map texture1 :attach "material"}))))
+       ($ :meshStandardMaterial
+          {:map texture1
+           :side three/DoubleSide
+           :attach "material"}))))
