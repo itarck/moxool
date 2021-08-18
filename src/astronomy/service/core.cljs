d98e5850f942e00dfffda6806328b0b1473cd421
itarck <itarck@gmail.com>
2021-08-18 14:41:28 +0800

    service core 的listen用字符串

--- src/astronomy/service/core.cljs
+++ src/astronomy/service/core.cljs
@@ -27,116 +27,115 @@
 
 
 (def processes
-  [{:listen []
-    :process-name "keyboard"
-    :service-fn s.keyboard-listener/init-service!}
-   {:listen [:user]
-    :process-name "user"
-    :service-fn s.user/init-service!}
-   {:listen [:astro-scene]
-    :process-name "astro-scene"
-    :service-fn s.astro-scene/init-service!}
-
-   {:listen [:tool]
-    :process-name "tool"
-    :handle-event-fn s.tool/handle-event}
-
-   {:listen [:universe-tool]
-    :process-name "universe-tool"
-    :service-fn s.universe-tool/init-service!}
-   {:listen [:clock-tool]
-    :process-name "clock-tool"
-    :service-fn s.clock-tool/init-service!}
-   {:listen [:info-tool]
-    :process-name "info-tool"
-    :service-fn s.info-tool/init-service!}
-
-   {:listen [:spaceship-camera-control :astro-scene.pub]
-    :process-name "spaceship-camera-control"
-    :handle-event-fn s.spaceship/handle-event}
-
-   {:listen [:ppt-tool]
-    :process-name "ppt-tool"
-    :service-fn s.ppt-tool/init-service!}
-   {:listen [:goto-celestial-tool]
-    :process-name "goto-celestial-tool"
-    :service-fn s.goto-tool/init-service!}
-   {:listen [:constellation-tool]
-    :process-name "constellation-tool"
-    :service-fn s.constellation-tool/init-service!}
-   {:listen [:atmosphere-tool]
-    :process-name "atmosphere-tool"
-    :service-fn s.atmosphere-tool/init-service!}
-   {:listen [:horizon-coordinate]
-    :process-name "horizon-coordinate"
-    :service-fn s.horizon-coordinate/init-service!}
-   {:listen [:astronomical-coordinate-tool]
-    :process-name "astronomical-coordinate-tool"
-    :handle-event-fn astronomical-coordinate-tool.h/handle-event}
-   {:listen [:terrestrial-coordinate-tool]
-    :process-name "terrestrial-coordinate-tool"
-    :service-fn s.terrestrial-coordinate-tool/init-service!}
-   {:listen [:astronomical-point-tool]
-    :process-name "astronomical-point-tool"
-    :handle-event-fn s.astronomical-point-tool/handle-event}
-   {:listen [:ruler-tool]
-    :process-name "ruler-tool"
-    :handle-event-fn s.ruler-tool/handle-event}
-   {:listen [:ecliptic]
-    :process-name "ecliptic"
-    :handle-event-fn ecliptic.h/handle-event}
-
-   {:listen [:planet-tool]
-    :process-name "planet-tool"
-    :handle-event-fn planet-tool.h/handle-event}
-   {:listen []
-    :process-name "camera"
-    :service-fn s.camera/init-service!}
-   {:listen [:mouse]
-    :process-name "mouse"
-    :service-fn s.mouse/init-service!}])
+  [#:process{:name "keyboard"
+             :listen []
+             :service-fn s.keyboard-listener/init-service!}
+   #:process{:name "user"
+             :listen ["user"]
+             :service-fn s.user/init-service!}
+   #:process{:name "astro-scene"
+             :listen ["astro-scene"]
+             :service-fn s.astro-scene/init-service!}
+   #:process{:name "tool"
+             :listen ["tool"]
+             :handle-event-fn s.tool/handle-event}
+   #:process{:name "universe-tool"
+             :listen ["universe-tool"]
+             :service-fn s.universe-tool/init-service!}
+   #:process{:name "clock-tool"
+             :listen ["clock-tool"]
+             :service-fn s.clock-tool/init-service!}
+   #:process{:name "info-tool"
+             :listen ["info-tool"]
+             :service-fn s.info-tool/init-service!}
+   #:process{:name "spaceship-camera-control"
+             :listen ["spaceship-camera-control" "astro-scene.pub"]
+             :handle-event-fn s.spaceship/handle-event}
+   #:process{:name "ppt-tool"
+             :listen ["ppt-tool"]
+             :service-fn s.ppt-tool/init-service!}
+   #:process{:name "goto-celestial-tool"
+             :listen ["goto-celestial-tool"]
+             :service-fn s.goto-tool/init-service!}
+   #:process{:name "constellation-tool"
+             :listen ["constellation-tool"]
+             :service-fn s.constellation-tool/init-service!}
+   #:process{:name "atmosphere-tool"
+             :listen ["atmosphere-tool"]
+             :service-fn s.atmosphere-tool/init-service!}
+   #:process{:name "horizon-coordinate"
+             :listen ["horizon-coordinate"]
+             :service-fn s.horizon-coordinate/init-service!}
+   #:process{:name "astronomical-coordinate-tool"
+             :listen ["astronomical-coordinate-tool"]
+             :handle-event-fn astronomical-coordinate-tool.h/handle-event}
+   #:process{:name "terrestrial-coordinate-tool"
+             :listen ["terrestrial-coordinate-tool"]
+             :service-fn s.terrestrial-coordinate-tool/init-service!}
+   #:process{:name "astronomical-point-tool"
+             :listen ["astronomical-point-tool"]
+             :handle-event-fn s.astronomical-point-tool/handle-event}
+   #:process{:name "ruler-tool"
+             :listen ["ruler-tool"]
+             :handle-event-fn s.ruler-tool/handle-event}
+   #:process{:name "ecliptic"
+             :listen ["ecliptic"]
+             :handle-event-fn ecliptic.h/handle-event}
+   #:process{:name "planet-tool"
+             :listen ["planet-tool"]
+             :handle-event-fn planet-tool.h/handle-event}
+   #:process{:name "camera"
+             :listen []
+             :service-fn s.camera/init-service!}
+   #:process{:name "mouse"
+             :listen ["mouse"]
+             :service-fn s.mouse/init-service!}])
 
 
-(defn init-service! [props {:keys [process-name process-chan handle-event-fn conn dom-atom] :as env}]
-  (go-loop []
-    (let [event (<! process-chan)
-          handler-env {:db @conn
-                       :dom @dom-atom}]
-      (try
-        (let [effect (handle-event-fn props handler-env event)]
-          (if (or (seq? effect) (vector? effect))
-            (doseq [eft effect]
-              (s.effect/handle-effect! eft env))
-            (s.effect/handle-effect! effect env)))
-        (catch js/Error e
-          (println process-name ": no handler found for event" (:event/action event)))))
-    (recur)))
+(defn init-service! [props env]
+  (let [{:keys [process-name process-chan handle-event-fn conn dom-atom]} env]
+    (go-loop []
+      (let [event (<! process-chan)
+            handler-env {:db @conn
+                         :dom @dom-atom}]
+        (try
+          (let [effect (handle-event-fn props handler-env event)]
+            (if (or (seq? effect) (vector? effect))
+              (doseq [eft effect]
+                (s.effect/handle-effect! eft env))
+              (s.effect/handle-effect! effect env)))
+          (catch js/Error e
+            (println process-name ": no handler found for event" (:event/action event)))))
+      (recur))))
 
 
 (defn init-service-center! [props env]
   ;; (println "!!!!! init service-center......")
   (let [{:keys [service-chan]} env
         process-dispatch-fn (fn [event]
-                              (keyword (namespace (:event/action event))))
+                              (namespace (:event/action event)))
         process-publication (async/pub service-chan process-dispatch-fn)]
 
-    (doseq [{:keys [service-fn handle-event-fn process-name listen]} processes]
+    (doseq [{:process/keys [service-fn handle-event-fn listen] process-name :process/name} processes]
       (let [process-chan (chan)]
         (doseq [l listen]
-          (async/sub process-publication l process-chan))
+          (async/sub process-publication (name l) process-chan))
         (when service-fn
           (service-fn props (-> env
                                 (assoc :process-chan process-chan)
                                 (assoc :process-name process-name))))
-        (when handle-event-fn 
+        (when handle-event-fn
           (init-service! props (-> env
                                    (assoc :process-chan process-chan)
                                    (assoc :process-name process-name)
-                                   (assoc :handle-event-fn handle-event-fn)))
-          )
-        ))
-
-    ;; (kick-start! env)
+                                   (assoc :handle-event-fn handle-event-fn))))))
 
     {:service-chan service-chan}))
 
+
+
+(let [{process-name :process/name
+       :process/keys [listen]} #:process{:name "ppt-tool"
+                                         :listen ["ppt-tool"]
+                                         :service-fn s.ppt-tool/init-service!}]
+  process-name)
