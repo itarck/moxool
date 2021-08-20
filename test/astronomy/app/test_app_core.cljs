22d2f6a3da880160bed193ef27a49e0dce9562d3
itarck <itarck@gmail.com>
2021-08-20 16:42:56 +0800

    测试性能

--- test/astronomy/app/test_app_core.cljs
+++ test/astronomy/app/test_app_core.cljs
@@ -1,15 +1,61 @@
 (ns astronomy.app.test-app-core
   (:require
    [astronomy.app.core :as app]
+   [cljs.core.async :refer [go >!]]
+   [posh.reagent :as p]
+   [film2.system.studio :as studio]
    [datascript.core :as d]))
 
 
-(def conn (:studio/conn app/studio))
+;; current studio in app core
 
-(def scene-system (:scene-system @(:studio/instance-atom app/studio)))
+(comment
+  (def solar-system1 (:scene-system @(:studio/instance-atom app/studio)))
 
-(def scene-conn (:system/conn scene-system))
 
-(def mercury (d/pull @scene-conn '[*] [:planet/name "mercury"]))
+  (def conn (get-in solar-system1 [:system/conn]))
+  (def service-chan (get-in solar-system1 [:system/service-chan]))
 
-(:planet/position-log mercury)
+
+  (def clock @(p/pull conn '[*] [:clock/name "default"]))
+  (def clock-tool @(p/pull conn '[*] [:tool/name "clock control 1"]))
+
+
+  (go (>! service-chan #:event{:action :clock-tool/start
+                               :detail {:clock-tool clock-tool
+                                        :clock clock}}))
+
+  (:clock/time-in-days @(p/pull conn '[*] [:clock/name "default"]))
+
+
+  (go (>! service-chan #:event{:action :clock-tool/stop
+                               :detail {:clock-tool clock-tool
+                                        :clock clock}})))
+
+
+
+;; 
+
+(comment
+
+  (def studio-2 (studio/create-app! {}))
+
+  (def solar-system2 (:scene-system @(:studio/instance-atom studio-2)))
+
+
+  (def conn2 (get-in solar-system2 [:system/conn]))
+  (def service-chan2 (get-in solar-system2 [:system/service-chan]))
+
+
+  (def clock2 @(p/pull conn2 '[*] [:clock/name "default"]))
+  (def clock-tool2 @(p/pull conn2 '[*] [:tool/name "clock control 1"]))
+
+  (go (>! service-chan2 #:event{:action :clock-tool/start
+                                :detail {:clock-tool clock-tool
+                                         :clock clock}}))
+
+  (go (>! service-chan2 #:event{:action :clock-tool/stop
+                                :detail {:clock-tool clock-tool
+                                         :clock clock}}))
+  
+  )
