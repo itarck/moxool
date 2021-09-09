f5183435c8bd62fedc4a39e389c5aad0d6ed944f
itarck <itarck@gmail.com>
2021-09-09 17:48:38 +0800

    测试

--- test/astronomy/model/test_planet.cljs
+++ test/astronomy/model/test_planet.cljs
@@ -3,50 +3,39 @@
    [cljs.test :refer-macros [deftest is testing run-tests]]
    [datascript.core :as d]
    [astronomy.objects.planet.m :as planet]
-   [astronomy.model.coordinate :as coordinate]
-   [astronomy.scripts.test-conn :refer [test-db11]]))
+   [astronomy.objects.planet.h :as planet.h :refer [handle-event]]
+   [astronomy.test-db :refer [test-db-full]]))
 
-
-(def planet-1
-  #:planet {:name "earth"
-            :chinese-name "地球"
-            :radius 2
-            :color "blue"
-            :star [:star/name "sun"]
-
-            :celestial/orbit #:circle-orbit {:star [:star/name "sun"]
-                                             :start-position [0 0 -500]
-                                             :axis [-1 2 0]
-                                             :period 365}
-            :celestial/spin #:spin {:axis [0 1 0]
-                                    :period 1}
-            :celestial/gltf #:gltf {:model-url "models/11-tierra/scene.gltf"
-                                    :model-scale [0.2 0.2 0.2]}
-
-            :object/scene [:scene/name "solar"]
-            :object/position [100 0 0]
-            :entity/type :planet})
-
+(def test-db test-db-full)
 
 ;; test model
 
-(def earth (d/pull test-db11 '[*] [:planet/name "earth"]))
-(def mercury (d/pull test-db11 '[*] [:planet/name "mercury"]))
+(def earth (d/pull test-db '[*] [:planet/name "earth"]))
+(def mercury (d/pull test-db '[*] [:planet/name "mercury"]))
 
-(def ac-1 (d/pull test-db11 '[*] [:coordinate/name "赤道天球坐标系"]))
+(def ac-1 (d/pull test-db '[*] [:coordinate/name "赤道天球坐标系"]))
+
 
 (deftest test-planet-1
-  (let [earth (d/pull test-db11 '[*] [:planet/name "earth"])]
-    (is (= (planet/cal-system-position-at-epoch test-db11 earth 2)
+  (let [earth (d/pull test-db '[*] [:planet/name "earth"])]
+    (is (= (planet/cal-system-position-at-epoch test-db earth 2)
            [439.7765559273832 190.30873698560643 -105.51499959752967]))
-    (is (= (planet/cal-system-position-at-epoch test-db11 earth 0)
+    (is (= (planet/cal-system-position-at-epoch test-db earth 0)
            [442.9497885783528 191.68192377598768 -88.40464973856325]))
-    (is (= (d/q planet/query-all-ids test-db11)
+    (is (= (d/q planet/query-all-ids test-db)
            [23 32 36 40 44 48]))
-    
-    (is (= (planet/cal-coordinate-position-at-epoch test-db11 mercury ac-1 0)
-           [-309.44533427268226 -123.98534769993685 122.15004660287056]))
+
     ))
+
+;; test handler
+
+(handle-event {} {} #:event {:action :planet/show-orbit
+                             :detail {:celestial earth
+                                      :show? true}})
+;; => (#:effect{:action :tx, :detail [{:db/id 26, :orbit/show? true}]})
 
 
 (run-tests)
+
+;; 集成测试
+
---
+++ test/astronomy/test_db.cljs
@@ -0,0 +1,110 @@
+(ns astronomy.test-db
+  (:require
+   [datascript.core :as d]
+   [posh.reagent :as p]
+   [methodology.model.user.backpack :as m.backpack]
+   [astronomy.conn.core :refer [create-empty-conn!] :as conn.core]
+   [astronomy.model.astro-scene :as m.astro-scene]
+   [astronomy.model.clock :as m.clock]
+   [astronomy.data.basic :as d.basic]
+   [astronomy.data.celestial :as d.celestial]
+   [astronomy.data.galaxy :as d.galaxy]
+   [astronomy.data.coordinate :as d.coordinate]
+   [astronomy.data.tool :as d.tool]
+   [astronomy.data.stars :as d.stars]
+   [astronomy.data.constellation :as d.constel]
+   [astronomy.data.misc :as d.misc]))
+
+;; processes
+
+
+(def tools
+  [{:db/id [:tool/name "ppt tool"]}
+   {:db/id [:tool/name "clock control 1"]}
+   {:db/id [:tool/name "goto celestial tool"]}
+   {:db/id [:tool/name "spaceship camera tool"]}
+   {:db/id [:tool/name "horizon-coordinate-tool"]}
+   {:db/id [:tool/name "terrestrial-coordinate-tool"]}
+   {:db/id [:tool/name "constellation-tool"]}
+   {:db/id [:tool/name "universe tool"]}
+   {:db/id [:tool/name "atmosphere-tool"]}
+   {:db/id [:tool/name "astronomical-coordinate-tool"]}])
+
+
+(defn kick-start! [conn]
+  (let [clock-id [:clock/name "default"]
+        astro-scene (d/pull @conn '[*] [:scene/name "solar"])
+        person (d/pull @conn '[*] [:person/name "dr who"])
+        backpack (d/pull @conn '[*] (-> person :person/backpack :db/id))]
+    (p/transact! conn (m.clock/set-clock-time-in-days-tx clock-id 0))
+    (p/transact! conn (m.astro-scene/refresh-tx @conn astro-scene))
+    (p/transact! conn (m.backpack/put-in-backpack-tx backpack tools))))
+
+
+;; dbs 
+
+(def test-db1
+  (let [conn (create-empty-conn!)]
+    (d/transact! conn d.basic/dataset1)
+    @conn))
+
+(def test-db2
+  (let [conn (create-empty-conn!)]
+    (d/transact! conn d.basic/dataset1)
+    (d/transact! conn d.celestial/dataset1)
+    (d/transact! conn d.celestial/dataset3)
+    (d/transact! conn d.coordinate/dataset1)
+    (d/transact! conn d.tool/dataset1)
+    @conn))
+
+(def test-db3
+  (let [conn (create-empty-conn!)]
+    (d/transact! conn d.basic/dataset1)
+    (d/transact! conn d.celestial/dataset1)
+    (d/transact! conn d.celestial/dataset2)
+    (d/transact! conn d.celestial/dataset3)
+    (d/transact! conn d.coordinate/dataset1)
+    (d/transact! conn d.tool/dataset1)
+    @conn))
+
+
+;; 经过kickstar
+(def test-db11
+  (let [conn (create-empty-conn!)]
+    (d/transact! conn d.basic/dataset1)
+    (d/transact! conn d.celestial/dataset1)
+    (d/transact! conn d.celestial/dataset3)
+    (d/transact! conn d.celestial/dataset4)
+    (d/transact! conn d.celestial/dataset5)
+    (d/transact! conn d.celestial/dataset-newton)
+    (d/transact! conn d.galaxy/dataset1)
+    (d/transact! conn d.coordinate/dataset1)
+    (d/transact! conn d.constel/dataset1)
+    (d/transact! conn d.tool/dataset1)
+    (d/transact! conn d.misc/dataset1)
+
+    (kick-start! conn)
+    @conn))
+
+
+;; 完整db
+(def test-db-full
+  (let [conn (create-empty-conn!)]
+    (d/transact! conn d.basic/dataset1)
+    (d/transact! conn d.celestial/dataset1)
+    (d/transact! conn d.celestial/dataset3)
+    (d/transact! conn d.celestial/dataset4)
+    (d/transact! conn d.galaxy/dataset1)
+    (d/transact! conn d.coordinate/dataset1)
+
+    (p/transact! conn d.stars/dataset1)
+    (d/transact! conn d.constel/dataset1)
+    (d/transact! conn d.constel/dataset2)
+    (d/transact! conn d.constel/dataset3)
+    (d/transact! conn d.tool/dataset1)
+    (d/transact! conn d.misc/dataset1)
+
+    (kick-start! conn)
+    @conn))
+
+
