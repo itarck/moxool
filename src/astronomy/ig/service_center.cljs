f888739c2c9f3f62f34b668ce9bb456fb192f871
itarck <itarck@gmail.com>
2021-09-14 12:05:26 +0800

    用process lib

--- src/astronomy/ig/service_center.cljs
+++ src/astronomy/ig/service_center.cljs
@@ -27,7 +27,6 @@
    [astronomy.objects.ecliptic.h :as ecliptic.h]
    [astronomy.objects.astronomical-coordinate.h :as astronomical-coordinate.h]
 
-
    [astronomy.tools.astronomical-coordinate-tool.h :as astronomical-coordinate-tool.h]
    [astronomy.tools.planet-tool.h :as planet-tool.h]
    [astronomy.tools.satellite-tool.h :as satellite-tool.h]
@@ -35,81 +34,81 @@
    ))
 
 
-(def processes
-  [#:process{:name "keyboard"
-             :listen []
-             :service-fn s.keyboard-listener/init-service!}
-   #:process{:name "user"
-             :listen ["user"]
-             :service-fn s.user/init-service!}
-   #:process{:name "astro-scene"
-             :listen ["astro-scene"]
-             :service-fn s.astro-scene/init-service!}
-   #:process{:name "planet"
-             :listen ["planet" "clock.pub"]
-             :handle-event-fn planet.h/handle-event}
-   #:process{:name "satellite"
-             :listen ["satellite"]
-             :handle-event-fn satellite.h/handle-event}
-   #:process{:name "astronomical-coordinate"
-             :listen ["astronomical-coordinate"]
-             :handle-event-fn astronomical-coordinate.h/handle-event}
-   #:process{:name "tool"
-             :listen ["tool"]
-             :handle-event-fn s.tool/handle-event}
-   #:process{:name "universe-tool"
-             :listen ["universe-tool"]
-             :service-fn s.universe-tool/init-service!}
-   #:process{:name "clock-tool"
-             :listen ["clock-tool"]
-             :service-fn s.clock-tool/init-service!}
-   #:process{:name "info-tool"
-             :listen ["info-tool"]
-             :service-fn s.info-tool/init-service!}
-   #:process{:name "spaceship-camera-control"
-             :listen ["spaceship-camera-control" "astro-scene.pub"]
-             :handle-event-fn s.spaceship/handle-event}
-   #:process{:name "ppt-tool"
-             :listen ["ppt-tool"]
-             :service-fn s.ppt-tool/init-service!}
-   #:process{:name "constellation-tool"
-             :listen ["constellation-tool"]
-             :service-fn s.constellation-tool/init-service!}
-   #:process{:name "atmosphere-tool"
-             :listen ["atmosphere-tool"]
-             :service-fn s.atmosphere-tool/init-service!}
-   #:process{:name "horizon-coordinate"
-             :listen ["horizon-coordinate"]
-             :service-fn s.horizon-coordinate/init-service!}
 
-   #:process{:name "astronomical-coordinate-tool"
-             :listen ["astronomical-coordinate-tool"]
-             :handle-event-fn astronomical-coordinate-tool.h/handle-event}
-   #:process{:name "terrestrial-coordinate-tool"
-             :listen ["terrestrial-coordinate-tool"]
-             :service-fn s.terrestrial-coordinate-tool/init-service!}
-   #:process{:name "astronomical-point-tool"
-             :listen ["astronomical-point-tool"]
-             :handle-event-fn s.astronomical-point-tool/handle-event}
-   #:process{:name "ruler-tool"
-             :listen ["ruler-tool"]
-             :handle-event-fn s.ruler-tool/handle-event}
-   #:process{:name "ecliptic"
-             :listen ["ecliptic"]
-             :handle-event-fn ecliptic.h/handle-event}
-   #:process{:name "planet-tool"
-             :listen ["planet-tool"]
-             :handle-event-fn planet-tool.h/handle-event}
-   #:process{:name "satellite-tool"
-             :listen ["satellite-tool"]
-             :handle-event-fn satellite-tool.h/handle-event}
+(def process-lib
+  {:keyboard #:process{:name "keyboard"
+                       :listen []
+                       :service-fn s.keyboard-listener/init-service!}
+   :user #:process{:name "user"
+                   :listen ["user"]
+                   :service-fn s.user/init-service!}
+   :astro-scene #:process{:name "astro-scene"
+                          :listen ["astro-scene"]
+                          :service-fn s.astro-scene/init-service!}
+   :planet #:process{:name "planet"
+                     :listen ["planet" "clock.pub"]
+                     :handle-event-fn planet.h/handle-event}
+   :satellite #:process{:name "satellite"
+                        :listen ["satellite"]
+                        :handle-event-fn satellite.h/handle-event}
+   :astronomical-coordinate #:process{:name "astronomical-coordinate"
+                                      :listen ["astronomical-coordinate"]
+                                      :handle-event-fn astronomical-coordinate.h/handle-event}
+   :tool #:process{:name "tool"
+                   :listen ["tool"]
+                   :handle-event-fn s.tool/handle-event}
+   :universe-tool #:process{:name "universe-tool"
+                            :listen ["universe-tool"]
+                            :service-fn s.universe-tool/init-service!}
+   :clock-tool #:process{:name "clock-tool"
+                         :listen ["clock-tool"]
+                         :service-fn s.clock-tool/init-service!}
+   :info-tool #:process{:name "info-tool"
+                        :listen ["info-tool"]
+                        :service-fn s.info-tool/init-service!}
+   :spaceship-camera-control #:process{:name "spaceship-camera-control"
+                                       :listen ["spaceship-camera-control" "astro-scene.pub"]
+                                       :handle-event-fn s.spaceship/handle-event}
+   :ppt-tool #:process{:name "ppt-tool"
+                       :listen ["ppt-tool"]
+                       :service-fn s.ppt-tool/init-service!}
+   :constellation-tool #:process{:name "constellation-tool"
+                                 :listen ["constellation-tool"]
+                                 :service-fn s.constellation-tool/init-service!}
+   :atmosphere-tool #:process{:name "atmosphere-tool"
+                              :listen ["atmosphere-tool"]
+                              :service-fn s.atmosphere-tool/init-service!}
+   :horizon-coordinate #:process{:name "horizon-coordinate"
+                                 :listen ["horizon-coordinate"]
+                                 :service-fn s.horizon-coordinate/init-service!}
 
-   #:process{:name "camera"
-             :listen []
-             :service-fn s.camera/init-service!}
-   #:process{:name "mouse"
-             :listen ["mouse"]
-             :service-fn s.mouse/init-service!}])
+   :astronomical-coordinate-tool #:process{:name "astronomical-coordinate-tool"
+                                           :listen ["astronomical-coordinate-tool"]
+                                           :handle-event-fn astronomical-coordinate-tool.h/handle-event}
+   :terrestrial-coordinate-tool #:process{:name "terrestrial-coordinate-tool"
+                                          :listen ["terrestrial-coordinate-tool"]
+                                          :service-fn s.terrestrial-coordinate-tool/init-service!}
+   :astronomical-point-tool #:process{:name "astronomical-point-tool"
+                                      :listen ["astronomical-point-tool"]
+                                      :handle-event-fn s.astronomical-point-tool/handle-event}
+   :ruler-tool #:process{:name "ruler-tool"
+                         :listen ["ruler-tool"]
+                         :handle-event-fn s.ruler-tool/handle-event}
+   :ecliptic #:process{:name "ecliptic"
+                       :listen ["ecliptic"]
+                       :handle-event-fn ecliptic.h/handle-event}
+   :planet-tool #:process{:name "planet-tool"
+                          :listen ["planet-tool"]
+                          :handle-event-fn planet-tool.h/handle-event}
+   :satellite-tool #:process{:name "satellite-tool"
+                             :listen ["satellite-tool"]
+                             :handle-event-fn satellite-tool.h/handle-event}
+   :camera #:process{:name "camera"
+                     :listen []
+                     :service-fn s.camera/init-service!}
+   :mouse #:process{:name "mouse"
+                    :listen ["mouse"]
+                    :service-fn s.mouse/init-service!}})
 
 
 (defn init-service! [props env]
@@ -153,23 +152,17 @@
     {:service-chan service-chan}))
 
 
-(def default-config
-  #:service {:processes processes
-             :props {:user {:db/id [:person/name "dr who"]}
-                     :astro-scene {:db/id [:scene/name "solar"]}
-                     :camera {:db/id [:camera/name "default"]}
-                     :spaceship-camera-control {:db/id [:spaceship-camera-control/name "default"]}}
-             :env {:service-chan (ig/ref ::chan)
-                   :conn (ig/ref ::conn)
-                   :state-atom (ig/ref ::state-atom)
-                   :dom-atom (ig/ref ::dom-atom)}})
-
 
 (defmethod ig/init-key :astronomy/service-center [_key config]
-  (let [{:service/keys [processes props env]} (merge default-config config)
+  (let [{:service/keys [processes props env]} config
+        process-kvs (if processes 
+                            (select-keys process-lib processes)
+                            process-lib)
         service-chan (get-in env [:service-chan])]
 
     (println "init service center start: " (js/Date))
-    (init-service-center! processes props env)
+    (init-service-center! (vals process-kvs) props env)
     (println "init service center end: " (js/Date))
     {:service-chan service-chan}))
+
+
