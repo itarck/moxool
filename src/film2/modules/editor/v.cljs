2b17abd8b93a9300b3cec4307eec2ac660874d1b
itarck <itarck@gmail.com>
2021-09-16 17:09:02 +0800

    工具栏

--- src/film2/modules/editor/v.cljs
+++ src/film2/modules/editor/v.cljs
@@ -7,50 +7,46 @@
    [posh.reagent :as p]))
 
 
-(defn FreeView [{:keys [editor]} {:keys [conn instance-atom service-chan] :as env}]
-  (let [editor-1 @(p/pull conn '[*] (:db/id editor))
-        status (:editor/status editor-1)]
-    [:div {:style {:position :absolute
-                   :height "100%"
-                   :width "100%"}}
-     (case status
-       :init [:p "init"]
-       :ready (get-in @instance-atom [:scene-system :ioframe-system/view]))]))
 
-
-(defn EditorView [{:keys [editor]} {:keys [conn instance-atom service-chan] :as env}]
+(defn IOFrameToolView [{:keys [editor]} {:keys [conn service-chan] :as env}]
   (let [editor-1 @(p/pull conn '[*] (:db/id editor))
         current-ioframe-id (get-in editor-1 [:editor/current-frame :db/id])
-        status (:editor/status editor-1)
         ioframes @(p/q ioframe.m/all-id-and-names-query conn)]
+    [:<>
+     [:> mt/Select {:value current-ioframe-id
+                    :onChange (fn [e]
+                                (let [new-value (j/get-in e [:target :value])]
+                                  (go (>! service-chan
+                                          #:event {:action :editor/change-current-ioframe
+                                                   :detail {:editor editor-1
+                                                            :ioframe {:db/id new-value}}}))))}
+      (for [[id name] ioframes]
+        ^{:key id}
+        [:> mt/MenuItem {:value id} name])]
+
+     [:input {:type :button
+              :value "load"
+              :on-click #(go (>! service-chan #:event{:action :editor/load-current-frame}))}]]))
+
+
+(defn EditorView [{:keys [editor] :as props} {:keys [conn instance-atom] :as env}]
+  (let [editor-1 @(p/pull conn '[*] (:db/id editor))
+        current-ioframe-id (get-in editor-1 [:editor/current-frame :db/id])]
     [:<>
      [:div {:style {:position :absolute
                     :top "0px"
                     :width "100%"
                     :height "80px"}}
-      [:p (str editor-1)]
-
-      [:> mt/Select {:value current-ioframe-id
-                     :onChange (fn [e]
-                                 (let [new-value (j/get-in e [:target :value])]
-                                   (go (>! service-chan
-                                           #:event {:action :editor/change-current-ioframe
-                                                    :detail {:editor editor-1
-                                                             :ioframe {:db/id new-value} }}))))}
-       (for [[id name] ioframes]
-         ^{:key id}
-         [:> mt/MenuItem {:value id} name])]
-      
-      #_[:input {:type :button
-               :value "pull"
-               :on-click #(go (>! service-chan #:event{:action :editor/pull-current-frame}))}]
-      [:input {:type :button
-               :value "load"
-               :on-click #(go (>! service-chan #:event{:action :editor/load-current-frame}))}]]
+      [:> mt/Grid {:container true :spacing 0}
+       [:> mt/Grid {:item true :xs 2}
+        [:div "模式"]
+        ]
+       [:> mt/Grid {:item true :xs 10}
+        [IOFrameToolView props env]]]]
      [:div {:style {:position :absolute
                     :top "80px"
                     :height "720px"
                     :width "1280px"}}
-      (case status
+      (case (:editor/status editor-1)
         :init [:p "init"]
         :ready (get-in @instance-atom [:ioframe current-ioframe-id :ioframe-system/view]))]]))
