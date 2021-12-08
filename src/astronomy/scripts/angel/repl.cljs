6677492531017a0c5feb036d5603fc7f39440bd2
itarck <itarck@gmail.com>
2021-12-08 19:43:40 +0800

    Add scene-3-3-v2

--- src/astronomy/scripts/angel/repl.cljs
+++ src/astronomy/scripts/angel/repl.cljs
@@ -221,3 +221,27 @@
     (api/save-db-file @conn db-url))
 ;;
   )
+
+
+(comment  ;; scene 3-3
+
+  (slib/init-tool! conn slib/all-tools)
+
+  (let [tools-3-3 [{:db/id [:tool/name "clock control 1"]}
+                   {:db/id [:tool/name "planet-tool"]}
+                   {:db/id [:tool/name "constellation-tool"]}
+                   {:db/id [:tool/name "astronomical-coordinate-tool"]}
+                   {:db/id [:tool/name "terrestrial-coordinate-tool"]}
+                   {:db/id [:tool/name "horizon-coordinate-tool"]}]]
+    (slib/init-tool! conn tools-3-3))
+
+  (slib/init-scene! conn planets-5)
+
+  (change-sun-light true)
+  (change-scene-ambient-light 0.1)
+  (re-frash-camera!)
+
+  (let [db-url "/frame/dev/scene-3-3-v2.fra"]
+    (api/save-db-file @conn db-url))
+;;
+  )
