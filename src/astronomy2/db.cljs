6e82436d3e0832d5f11648467e9d9f82e37bbef7
itarck <itarck@gmail.com>
2022-01-11 17:42:51 +0800

    Replace model of base/model

--- src/astronomy2/db.cljs
+++ src/astronomy2/db.cljs
@@ -1,33 +1,42 @@
 (ns astronomy2.db
   (:require
    [datascript.core :as d]
-   [fancoil.base :as base]
    [astronomy2.system :as sys]))
 
 
-(def schema
-  (::sys/schema (sys/init {})))
+(def temp-sys 
+  (sys/init {}))
 
+(def schema
+  (::sys/schema temp-sys))
+
+(def model
+  (::sys/model temp-sys))
 
 (defn create-db []
   (let [conn (d/create-conn schema)
-        tx  [(base/model {} :framework/create {})
-             (base/model {} :scene/create {:scene/background "black"})
-             (base/model {} :user/create {})
-             (base/model {} :backpack/create {:backpack/cell [{:backpack-cell/index 0
-                                                               :backpack-cell/tool -1}
-                                                              {:backpack-cell/index 1
-                                                               :backpack-cell/tool -2}]})
-             (base/model {} :object/create #:object{:scale [1 1 5]})
-             (base/model {} :object/create #:object{:scale [1 1 1]
-                                                    :position [-3 0 0]})
-             (base/model {} :tool/create #:tool{:db/id -1
-                                                :name "universe tool"
-                                                :chinese-name "宇宙"
-                                                :icon "image/moxool/universe.webp"})
-             (base/model {} :tool/create #:tool{:db/id -2
-                                                :name "clock tool"
-                                                :chinese-name "时钟"
-                                                :icon "image/moxool/clock.jpg"})]]
+        tx [(model :framework/create {})
+            (model :scene/create {:scene/background "black"})
+            (model :user/create {})
+            (model :backpack/create {:backpack/cell [{:backpack-cell/index 0
+                                                      :backpack-cell/tool -1}
+                                                     {:backpack-cell/index 1
+                                                      :backpack-cell/tool -2}]})
+            (model :object/create #:object{:scale [1 1 5]})
+            (model :object/create #:object{:scale [1 1 1]
+                                           :position [-3 0 0]})
+            (model :tool/create #:tool{:db/id -1
+                                       :name "universe tool"
+                                       :chinese-name "宇宙"
+                                       :icon "image/moxool/universe.webp"})
+            (model :tool/create #:tool{:db/id -2
+                                       :name "clock tool"
+                                       :chinese-name "时钟"
+                                       :icon "image/moxool/clock.jpg"})]]
     (d/transact! conn tx)
     @conn))
+
+
+(comment 
+  (create-db)
+  )
