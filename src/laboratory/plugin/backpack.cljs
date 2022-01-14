1eaf3b34877cc0cbabd6a77b5c507ff49a19a611
itarck <itarck@gmail.com>
2022-01-14 17:18:59 +0800

    Add backpack model test

--- src/laboratory/plugin/backpack.cljs
+++ src/laboratory/plugin/backpack.cljs
@@ -1,5 +1,6 @@
 (ns laboratory.plugin.backpack
   (:require
+   [cljs.spec.alpha :as s]
    [laboratory.base :as base]
    [datascript.core :as d]
    [posh.reagent :as p]))
@@ -20,48 +21,66 @@
   {:backpack/name {:db/unique :db.unique/identity}
    :backpack/owner {:db/valueType :db.type/ref :db/cardinality :db.cardinality/one}
    :backpack/active-cell {:db/valueType :db.type/ref :db/cardinality :db.cardinality/one}
-   :backpack/cell {:db/valueType :db.type/ref
-                   :db/cardinality :db.cardinality/many
-                   :db/isComponent true}
+   :backpack/cells {:db/valueType :db.type/ref
+                    :db/cardinality :db.cardinality/many
+                    :db/isComponent true}
    :backpack-cell/tool {:db/valueType :db.type/ref :db/cardinality :db.cardinality/one}})
 
 ;; spec
 
+(defmethod base/spec :backpack/spec
+  [_ _]
+  (s/def :backpack-cell/backpack-cell (s/keys :req [:db/id :backpack-cell/index]))
+  (s/def :backpack/cells (s/coll-of :backpack-cell/backpack-cell))
+  (s/def :backpack/backpack (s/keys :req [:db/id :backpack/name :backpack/cells])))
 
 ;; model
 
 (defmethod base/model :backpack/create
-  [_ _ props]
+  [_ _ entity]
   (let [default #:backpack {:name "default"
                             :user/_backpack [:user/name "default"]
                             :owner [:user/name "default"]
                             :cell (vec (for [i (range 12)]
                                          #:backpack-cell{:index i}))}]
-    (merge default props)))
+    (merge default entity)))
 
-(defmethod base/model :backpack/find-nth-cell
-  [_ _ {:keys [backpack nth-cell]}]
-  (->
-   (filter (fn [cell] (= (:backpack-cell/index cell) nth-cell))
-           (:backpack/cell backpack))
-   first))
+(defmethod base/model :backpack/pull
+  [_ _ {:keys [db entity]}]
+  (s/assert :db/entity entity)
+  (d/pull db '[*] (:db/id entity)))
 
-(defmethod base/model :backpack/find-nth-cell2
+(defmethod base/model :backpack/sync
+  [_ _ {:keys [db entity]}]
+  (if (s/valid? :backpack/backpack entity)
+    entity
+    (base/model :backpack/pull {:entity entity
+                                :db db})))
+
+(defmethod base/model :backpack/query-nth-cell
   [_ _ {:keys [db backpack nth-cell]}]
   (let [cell-id (d/q '[:find ?cell-id .
                        :where
-                       [?bp-id :backpack/cell ?cell-id]
+                       [?bp-id :backpack/cells ?cell-id]
                        [?cell-id :backpack-cell/index ?nth-cell]
                        :in $ ?bp-id ?nth-cell]
                      db (:db/id backpack) nth-cell)]
     (when cell-id
       {:db/id cell-id})))
 
-(defmethod base/model :backpack/put-in-cell-tx
-  [_ _ {:keys [backpack nth-cell tool]}]
-  (let [cell (base/model {} :backpack/find-nth-cell {:backpack backpack
-                                                     :nth-cell nth-cell})]
-    [[:db/add (:db/id cell) :backpack-cell/tool (:db/id tool)]]))
+(defmethod base/model :backpack/get-nth-cell
+  [_ _ {:keys [backpack nth-cell]}]
+  (s/assert :backpack/backpack backpack)
+  (->
+   (filter (fn [cell] (= (:backpack-cell/index cell) nth-cell))
+           (:backpack/cells backpack))
+   first))
+
+(defmethod base/model :backpack/sorted-cells
+  [_ _ {:keys [backpack]}]
+  (s/assert (s/keys :req [:backpack/cells]) backpack)
+  (let [cells (:backpack/cells backpack)]
+    (sort-by :backpack-cell/index cells)))
 
 (defmethod base/model :backpack/active-cell-tx
   [_ _ {:keys [backpack cell]}]
@@ -71,42 +90,28 @@
   [_ _ {:keys [backpack]}]
   [[:db.fn/retractAttribute (:db/id backpack) :backpack/active-cell]])
 
-(defmethod base/model :backpack/put-in-backpack-tx
+(defmethod base/model :backpack/put-in-nth-cell-tx
+  [_ _ {:keys [backpack nth-cell tool]}]
+  (let [cell (base/model {} :backpack/get-nth-cell {:backpack backpack
+                                                    :nth-cell nth-cell})]
+    [[:db/add (:db/id cell) :backpack-cell/tool (:db/id tool)]]))
+
+(defmethod base/model :backpack/init-tools-tx
   [_ _ {:keys [backpack tools]}]
-  (apply concat
-         (for [i (range (count tools))]
-           (base/model {} :backpack/put-in-cell-tx {:backpack backpack
-                                                    :nth-cell i
-                                                    :tool (get tools i)}))))
-
-(defmethod base/model :backpack/clear-backpack-tx
-  [_ _ {:keys [db backpack]}]
-  (let [backpack-1 (d/pull db '[{:backpack/cell [*]}] (:db/id backpack))
-        tx (vec (for [cell (:backpack/cell backpack-1)]
-                  [:db.fn/retractAttribute (:db/id cell) :backpack-cell/tool]))]
-    tx))
-
+  (vec (apply concat
+              (for [i (range (count tools))]
+                (base/model {} :backpack/put-in-nth-cell-tx
+                            {:backpack backpack
+                             :nth-cell i
+                             :tool (get tools i)})))))
 
 ;; handle 
-
-#_(defmethod base/handle :backpack/click-cell
-  [{:keys [model]} _ {:request/keys [body]}]
-  (let [{:keys [user cell active-cell backpack]} body
-        tx (if (= (:db/id active-cell) (:db/id cell))
-             (concat
-              (model :backpack/deactive-cell-tx {:backpack backpack})
-              (model :user/drop-tool-tx {:user user}))
-             (concat
-              (model :backpack/active-cell-tx {:backpack backpack
-                                               :cell cell})
-              (model :user/select-tool-tx {:user user
-                                           :tool (:backpack-cell/tool cell)})))]
-    {:posh/tx tx}))
 
 (defmethod base/handle :backpack/click-cell
   [{:keys [model]} _ {:request/keys [body] db :posh/db}]
   (let [{:keys [backpack cell]} body
-        backpack (d/pull db '[*] (:db/id backpack))
+        backpack (model :backpack/sync {:entity backpack
+                                        :db db})
         active-cell (:backpack/active-cell backpack)
         tx (if (= (:db/id active-cell) (:db/id cell))
              (model :backpack/deactive-cell-tx {:backpack backpack})
@@ -118,20 +123,21 @@
 ;; subscribe 
 
 (defmethod base/subscribe :backpack/pull
-  [{:keys [pconn]} _ {:keys [pattern id]}]
-  (let [pt (or pattern '[* {:backpack/cell [{:backpack-cell/tool [*]}]
-                            :user/_backpack [:db/id]}])]
-    (p/pull pconn pt id)))
+  [{:keys [pconn]} _ {:keys [entity pattern]}]
+  (s/assert :db/entity entity)
+  (let [pt (or pattern '[* {:user/_backpack [:db/id]}])]
+    (p/pull pconn pt (:db/id entity))))
 
 ;; view
 
 (defmethod base/view :backpack/view
   [{:keys [subscribe dispatch]} _ backpack]
-  (let [bp @(subscribe :backpack/pull {:id (:db/id backpack)})
-        user @(subscribe :db/pull {:id (get-in bp [:user/_backpack 0 :db/id])})
+  (s/assert :db/entity backpack)
+  (let [bp @(subscribe :backpack/pull {:entity backpack})
+        user @(subscribe :db/pull {:entity (get-in bp [:user/_backpack 0])})
         active-cell (:backpack/active-cell bp)]
     [:div {:class "d-flex justify-content-center astronomy-backpack"}
-     (for [cell (:backpack/cell bp)]
+     (for [cell (:backpack/cells bp)]
        (let [tool (:backpack-cell/tool cell)
              style (if (= (:db/id active-cell) (:db/id cell))
                      "astronomy-cell astronomy-cell-active"
--- test/laboratory/plugin/test_backpack.cljs
+++ test/laboratory/plugin/test_backpack.cljs
@@ -1,5 +1,6 @@
 (ns laboratory.plugin.test-backpack
   (:require
+   [cljs.spec.alpha :as s]
    [laboratory.dbs.dev :as dbs.dev]
    [laboratory.system.zero :as zero]
    [cljs.test :refer-macros [deftest is testing run-tests]]
@@ -13,37 +14,72 @@
 (def initial-tx
   [#:user {:name "default"}
    #:backpack {:name "default"
-               :user/_backpack [:user/name "default"]
-               :cell (vec (for [i (range 12)]
-                            #:backpack-cell{:index i}))}])
+               :cells (vec (for [i (range 12)]
+                             #:backpack-cell{:index i}))}])
 
 (def test-db1
   (helper/create-initial-db initial-tx))
 
 ;; spec 
 
-(def spec 
+(def spec
   (helper/create-spec-unit))
 
 (deftest test-spec-unit
   (let [spec (helper/create-spec-unit)]
     (testing "testing spec unit"
-      (is (spec :valid? :db/id 345)))))
-
+      (is (spec :valid? :backpack/backpack
+                #:backpack {:db/id -1
+                            :name "default"
+                            :cells [#:backpack-cell {:index 0
+                                                     :db/id -10}
+                                    #:backpack-cell {:index 1
+                                                     :db/id -11}]})))))
 
 ;; model 
 
 (def model
   (helper/create-model-unit))
 
+
 (deftest test-model-unit
-  (let [model (helper/create-model-unit)]
-    (testing "testing model unit"
-      (is (= (model :backpack/find-nth-cell2
-                    {:db test-db1
-                     :backpack {:db/id [:backpack/name "default"]}
-                     :nth-cell 0})
-             {:db/id 3})))))
+  (testing ":backpack/pull"
+    (let [bp (model :backpack/pull {:db test-db1
+                                    :entity {:db/id [:backpack/name "default"]}})]
+      (is (s/valid? :backpack/backpack bp))
+      (is (= 12 (count (model :backpack/sorted-cells {:backpack bp}))))))
+
+  (testing ":backpack/query-nth-cell"
+    (is (= (model :backpack/query-nth-cell
+                  {:db test-db1
+                   :backpack {:db/id [:backpack/name "default"]}
+                   :nth-cell 0})
+           {:db/id 3})))
+  
+  (testing ":backpack/active and deactive cell"
+    (let [bp (model :backpack/pull {:db test-db1
+                                    :entity {:db/id [:backpack/name "default"]}})
+          cell (first (model :backpack/sorted-cells {:backpack bp}))]
+      (is (= (model :backpack/active-cell-tx {:backpack bp :cell cell})
+             [[:db/add 2 :backpack/active-cell 3]]))
+      (is (= (model :backpack/deactive-cell-tx {:backpack bp})
+             [[:db.fn/retractAttribute 2 :backpack/active-cell]]))))
+  
+  (testing ":backpack/put-in-nth-cell-tx"
+    (let [bp (model :backpack/pull {:db test-db1
+                                    :entity {:db/id [:backpack/name "default"]}})]
+      (is (= (model :backpack/put-in-nth-cell-tx {:backpack bp
+                                                  :nth-cell 0
+                                                  :tool {:db/id -100}})
+             [[:db/add 3 :backpack-cell/tool -100]]))))
+  
+  (testing ":backpack/init-tools-tx"
+    (let [bp (model :backpack/pull {:db test-db1
+                                    :entity {:db/id [:backpack/name "default"]}})]
+      (is (= [[:db/add 3 :backpack-cell/tool -100] [:db/add 4 :backpack-cell/tool -101]]
+             (model :backpack/init-tools-tx {:backpack bp
+                                             :tools [{:db/id -100}
+                                                     {:db/id -101}]}))))))
 
 ;; handle 
 
@@ -55,12 +91,17 @@
 (def test-db
   (dbs.dev/create-dev-db1))
 
+(def system
+  (helper/create-event-system test-db))
+
+(def subscribe
+  (::zero/subscribe system))
+
+
 (deftest test-subscribe-unit
   (testing "subscribe backpack/pull"
-    (let [system (helper/create-event-system test-db)
-          {::zero/keys [subscribe spec]} system]
-      (is (spec :valid? :db/entity
-                @(subscribe :backpack/pull {:id [:backpack/name "default"]}))))))
+    (is (spec :valid? :db/entity
+              @(subscribe :backpack/pull {:entity {:db/id [:backpack/name "default"]}})))))
 
 
 
@@ -71,24 +112,13 @@
 (run-tests)
 
 
-(comment
-
-  (let [spec (helper/create-spec-unit)]
-    (spec :valid? :user/backpack {:db/id 34}))
-
-
-  (let [model (helper/create-model-unit)]
-    (model :user/select-tool-tx {:user {:db/id 1}
-                                 :tool {:db/id 2}}))
-
-  (let [handle (helper/create-handle-unit)]
-    (handle :backpack/click-cell))
-
-  (let [handle (helper/create-handle-unit)]
-    (handle :backpack/click-cell
-            {:request/body {:user {:db/id 1}
-                            :backpack {:db/id 2}
-                            :cell {:db/id 3}
-                            :active-cell {:db/id 3}}}))
-
+(comment 
+  
+  (let [bp (model :backpack/pull {:db test-db1
+                                  :entity {:db/id [:backpack/name "default"]}})]
+    (is (= [[:db/add 3 :backpack-cell/tool -100] [:db/add 4 :backpack-cell/tool -101]]
+           (model :backpack/init-tools-tx {:backpack bp
+                                           :tools [{:db/id -100}
+                                                   {:db/id -101}]}))))
+  ;; 
   )
